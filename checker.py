dae6fd4e4ce31a82b23d5b6f7c50470e798d6964
BirbAutoma <birbautoma@localhost.localdomain>
2020-03-06 21:50:45 +0100

    Generate entire sudoku board with missing numberinos

--- checker.py
+++ checker.py
@@ -3,13 +3,26 @@ import itertools
 
 def cellFiller(cella):
     for i in range(9):
-        cella.append(random.sample(range(1,10), random.randint(3,5)))
+        cella.append(random.sample(range(1,10), 9))
     return cella
+
+def sudokuGenerator(cella):
+    for row in cella:
+        for col in range(9):
+            removeCol = random.randint(0,1)
+            if(removeCol == 0):
+                row[col] = 0
+    return cella
+
 
 def grouper(iterable, n, fillValue=None):
     args = [iter(iterable)] * n
     return itertools.zip_longest(*args, fillvalue=fillValue)
 
 cell = cellFiller([])
-for block in grouper(cell,3):
+
+sudoku = sudokuGenerator(cell)
+#for block in grouper(cell,3):
+#   print(block)
+for block in sudoku:
     print(block)
