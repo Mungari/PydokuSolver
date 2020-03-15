99f776edcd12392d0b31b5be158718fa61c1bbd8
BirbAutoma <birbautoma@localhost.localdomain>
2020-03-15 21:26:54 +0100

    Code works, trying to bypass recursion

--- checker.py
+++ checker.py
@@ -2,19 +2,48 @@ import random
 import itertools
 import numpy as np
 
+s = 3
+divs = 3
+num_list = [k for k in range(1, s+1)]
+def Diff(li1, li2, li3): 
+    #li_dif = [i for i in li1 + li2 + li3 if i not in li1 or i not in li2 or i not in li3] 
+    return list((set(li1) - set(li2)) - set(li3)) 
+
+def get_square(x,y, grid):
+    return grid[x*divs:x*divs+divs, y*divs:y*divs+divs]
+
+def get_row(x,grid):
+    return list(grid[x])
+
+def get_col(y,grid):
+    return list(grid[:, y])
+
 def cellFiller(cella):
-    for row in range(9):
-        cella[row] = random.sample(range(1,10), 9)
-        for col in range(9):
-            num_to_add = random.randint(1,9)
-            passed = False
-            while(not passed):
-                if(num_to_add in cella[:, col]):
-                    passed = True
+    for row in range(s):
+        for col in range(s):
+            tried = 0
+            potential_list = Diff(num_list, get_row(row, cella), get_col(col, cella))
+            while(True):
+                if(potential_list):
+                    potential_num = random.choice(potential_list)
+                    #print(potential_num)
+                    if(potential_num not in get_square(row//divs, col//divs, cella)):
+                        cella[row][col] = potential_num
+                        break
+                    else:
+                        tried += 1
+                        if(potential_list.__len__() == tried):
+                            print("unsolvable 1")
+                            tmp = np.ndarray((s,s), int)
+                            tmp.fill(0)
+                            cellFiller(tmp)
+                        else:
+                            continue
                 else:
-                    print("ciao")
-                    passed = False
-        #cella.append(random.sample(range(1,10), 9))
+                    tmp = np.ndarray((s,s), int)
+                    tmp.fill(0)
+                    cellFiller(tmp)
+                    print("unsolvable 2")
     return cella
 
 def sudokuGenerator(cella):
@@ -30,11 +59,9 @@ def sudokuGenerator(cella):
     args = [iter(iterable)] * n
     return itertools.zip_longest(*args, fillvalue=fillValue)
 '''
-cell = np.ndarray((9,9), int)
+cell = np.ndarray((s,s), int)
 cell.fill(0)
 cellFiller(cell)
-sudoku = sudokuGenerator(cell)
+print(cell)
 #for block in grouper(cell,3):
 #   print(block)
-for block in sudoku:
-    print(block)
